a6925e4813ed5313fa94fc62f31941ac4e6eded7
Alex Young <foray1010@users.noreply.github.com>
2019-06-03 23:00:13 +0800

    feat: accept `useCapture` or `options` as 4th argument

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,39 @@
+import {shallowEqual} from 'fast-equals'
 import * as React from 'react'
+
+const useShallowMemorize = <T>(value: T): T => {
+  const ref = React.useRef<T>(value)
+
+  if (!shallowEqual(value, ref.current)) {
+    ref.current = value
+  }
+
+  return ref.current
+}
 
 function useEventListener<KD extends keyof DocumentEventMap>(
   element: Document | void,
   eventType: KD,
-  listener: (this: Document, evt: DocumentEventMap[KD]) => void
+  listener: (this: Document, evt: DocumentEventMap[KD]) => void,
+  options?: boolean | AddEventListenerOptions
 ): void
 function useEventListener<KH extends keyof HTMLElementEventMap>(
   element: HTMLElement | void,
   eventType: KH,
-  listener: (this: HTMLElement, evt: HTMLElementEventMap[KH]) => void
+  listener: (this: HTMLElement, evt: HTMLElementEventMap[KH]) => void,
+  options?: boolean | AddEventListenerOptions
 ): void
 function useEventListener<KW extends keyof WindowEventMap>(
   element: Window | void,
   eventType: KW,
-  listener: (this: Window, evt: WindowEventMap[KW]) => void
+  listener: (this: Window, evt: WindowEventMap[KW]) => void,
+  options?: boolean | AddEventListenerOptions
 ): void
 function useEventListener(
   element: Document | HTMLElement | Window | void,
   eventType: string,
-  listener: (evt: Event) => void
+  listener: (evt: Event) => void,
+  options?: boolean | AddEventListenerOptions
 ): void
 
 function useEventListener<
@@ -31,10 +46,13 @@ function useEventListener<
   listener: (
     this: typeof element,
     evt: DocumentEventMap[KD] | HTMLElementEventMap[KH] | WindowEventMap[KW] | Event
-  ) => void
+  ) => void,
+  options?: boolean | AddEventListenerOptions
 ): void {
   const listenerRef = React.useRef(listener)
   listenerRef.current = listener
+
+  const memorizedOptions = useShallowMemorize(options)
 
   React.useEffect(() => {
     if (!element) return undefined
@@ -43,12 +61,12 @@ function useEventListener<
     const wrappedListener: typeof listenerRef.current = (evt) =>
       listenerRef.current.call(element, evt)
 
-    element.addEventListener(eventType, wrappedListener)
+    element.addEventListener(eventType, wrappedListener, memorizedOptions)
 
     return () => {
-      element.removeEventListener(eventType, wrappedListener)
+      element.removeEventListener(eventType, wrappedListener, memorizedOptions)
     }
-  }, [element, eventType])
+  }, [element, eventType, memorizedOptions])
 }
 
 export default useEventListener
