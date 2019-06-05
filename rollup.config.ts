1ac8ef42eac6150750b7c6481fcf35690b3705ae
Alex Young <foray1010@users.noreply.github.com>
2019-06-05 21:11:13 +0800

    fix: enable tree shaking for package `fast-equals` (#5)

---
+++ rollup.config.ts
@@ -0,0 +1,21 @@
+import babel from 'rollup-plugin-babel'
+import replace from 'rollup-plugin-replace'
+import {terser} from 'rollup-plugin-terser'
+
+export default ['cjs', 'esm'].map((build) => ({
+  input: 'src/index.ts',
+  output: {
+    file: `dist/index.${build}.js`,
+    format: build,
+    sourcemap: true
+  },
+  plugins: [
+    babel({
+      extensions: ['.ts']
+    }),
+    replace({
+      'fast-equals': `fast-equals/dist/fast-equals.${build}`
+    }),
+    terser()
+  ]
+}))
