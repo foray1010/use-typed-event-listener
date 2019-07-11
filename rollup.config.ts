774502b6169fc9aa252125d7141cb173db456f42
Alex Young <foray1010@users.noreply.github.com>
2019-07-11 18:28:40 +0800

    build: support single declaration file

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,22 +1,30 @@
 // @ts-ignore
 import babel from 'rollup-plugin-babel'
+import dts from 'rollup-plugin-dts'
 import replace from 'rollup-plugin-replace'
 import {terser} from 'rollup-plugin-terser'
 
-export default ['cjs', 'esm'].map((build) => ({
-  input: 'src/index.ts',
-  output: {
-    file: `dist/index.${build}.js`,
-    format: build,
-    sourcemap: true
-  },
-  plugins: [
-    babel({
-      extensions: ['.ts']
-    }),
-    replace({
-      'fast-equals': `fast-equals/dist/fast-equals.${build}`
-    }),
-    terser()
-  ]
-}))
+export default [
+  ...['cjs', 'esm'].map((build) => ({
+    input: 'src/index.ts',
+    output: {
+      file: `dist/index.${build}.js`,
+      format: build,
+      sourcemap: true
+    },
+    plugins: [
+      babel({
+        extensions: ['.ts']
+      }),
+      replace({
+        'fast-equals': `fast-equals/dist/fast-equals.${build}`
+      }),
+      terser()
+    ]
+  })),
+  {
+    input: 'dist-dts/index.d.ts',
+    output: {file: 'dist/index.d.ts'},
+    plugins: [dts()]
+  }
+]
