1369faa09202676d23805b3718fbb208a4fc31a0
Alex Young <foray1010@users.noreply.github.com>
2020-06-20 21:27:05 +0800

    refactor: clearer rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,40 +1,54 @@
 // @ts-ignore
 import babel, { getBabelOutputPlugin } from '@rollup/plugin-babel'
-import replace from '@rollup/plugin-replace'
-import type { ModuleFormat, RollupOptions } from 'rollup'
+import fastEqualsPkg from 'fast-equals/package.json'
+import type { RollupOptions } from 'rollup'
 import dts from 'rollup-plugin-dts'
 import { terser } from 'rollup-plugin-terser'
 
-import packageJson from './package.json'
+import pkg from './package.json'
 
-const formats: ModuleFormat[] = ['cjs', 'esm']
+const outputDir = 'dist'
 
 const rollupOptions: RollupOptions[] = [
-  ...formats.map(
-    (format): RollupOptions => ({
-      external: Object.keys(packageJson.dependencies),
-      input: 'src/index.ts',
-      output: {
-        file: `dist/index.${format}.js`,
-        format,
+  {
+    external: Object.keys(pkg.dependencies),
+    input: 'src/index.ts',
+    output: [
+      {
+        dir: outputDir,
+        entryFileNames: '[name].[format].js',
+        format: 'cjs',
+        paths: {
+          'fast-equals': `fast-equals/${fastEqualsPkg.main}`,
+        },
         plugins: [getBabelOutputPlugin()],
         sourcemap: true,
       },
-      plugins: [
-        replace({
-          'fast-equals': `fast-equals/dist/fast-equals.${format}`,
-        }),
-        babel({
-          babelHelpers: 'runtime',
-          extensions: ['.js', '.ts'],
-        }),
-        terser(),
-      ],
-    }),
-  ),
+      {
+        dir: outputDir,
+        entryFileNames: '[name].esm.js',
+        format: 'esm',
+        paths: {
+          'fast-equals': `fast-equals/${fastEqualsPkg.module}`,
+        },
+        plugins: [getBabelOutputPlugin()],
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      babel({
+        babelHelpers: 'runtime',
+        extensions: ['.js', '.ts'],
+      }),
+      terser(),
+    ],
+  },
   {
     input: 'build/dts/index.d.ts',
-    output: { file: 'dist/index.d.ts' },
+    output: {
+      dir: outputDir,
+      entryFileNames: '[name].ts',
+    },
     plugins: [dts()],
   },
 ]
