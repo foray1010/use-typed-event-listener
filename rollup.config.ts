0760f553629650dc816c42298ad08c29d9f213d0
Alex Young <foray1010@users.noreply.github.com>
2020-06-20 18:15:45 +0800

    build: properly add typescript checking on rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,35 +1,42 @@
 // @ts-ignore
 import babel, { getBabelOutputPlugin } from '@rollup/plugin-babel'
 import replace from '@rollup/plugin-replace'
+import type { ModuleFormat, RollupOptions } from 'rollup'
 import dts from 'rollup-plugin-dts'
 import { terser } from 'rollup-plugin-terser'
 
 import packageJson from './package.json'
 
-export default [
-  ...['cjs', 'esm'].map((build) => ({
-    external: Object.keys(packageJson.dependencies),
-    input: 'src/index.ts',
-    output: {
-      file: `dist/index.${build}.js`,
-      format: build,
-      plugins: [getBabelOutputPlugin()],
-      sourcemap: true,
-    },
-    plugins: [
-      replace({
-        'fast-equals': `fast-equals/dist/fast-equals.${build}`,
-      }),
-      babel({
-        babelHelpers: 'runtime',
-        extensions: ['.js', '.ts'],
-      }),
-      terser(),
-    ],
-  })),
+const formats: ModuleFormat[] = ['cjs', 'esm']
+
+const rollupOptions: RollupOptions[] = [
+  ...formats.map(
+    (format): RollupOptions => ({
+      external: Object.keys(packageJson.dependencies),
+      input: 'src/index.ts',
+      output: {
+        file: `dist/index.${format}.js`,
+        format,
+        plugins: [getBabelOutputPlugin()],
+        sourcemap: true,
+      },
+      plugins: [
+        replace({
+          'fast-equals': `fast-equals/dist/fast-equals.${format}`,
+        }),
+        babel({
+          babelHelpers: 'runtime',
+          extensions: ['.js', '.ts'],
+        }),
+        terser(),
+      ],
+    }),
+  ),
   {
     input: 'build/dts/index.d.ts',
     output: { file: 'dist/index.d.ts' },
     plugins: [dts()],
   },
 ]
+
+export default rollupOptions
