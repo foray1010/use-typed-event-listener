38d081d31f388b55a3c2bfac38da175b8f475fd2
Alex Young <foray1010@users.noreply.github.com>
2020-06-25 10:35:29 +0800

    fix: allow undefined element

--- src/index.ts
+++ src/index.ts
@@ -12,25 +12,25 @@ const useShallowMemorize = <T>(value: T): T => {
 }
 
 function useEventListener<KD extends keyof DocumentEventMap>(
-  element: Document | null | void,
+  element: Document | null | undefined,
   eventType: KD,
   listener: (this: Document, evt: DocumentEventMap[KD]) => void,
   options?: boolean | AddEventListenerOptions,
 ): void
 function useEventListener<KH extends keyof HTMLElementEventMap>(
-  element: HTMLElement | null | void,
+  element: HTMLElement | null | undefined,
   eventType: KH,
   listener: (this: HTMLElement, evt: HTMLElementEventMap[KH]) => void,
   options?: boolean | AddEventListenerOptions,
 ): void
 function useEventListener<KW extends keyof WindowEventMap>(
-  element: Window | null | void,
+  element: Window | null | undefined,
   eventType: KW,
   listener: (this: Window, evt: WindowEventMap[KW]) => void,
   options?: boolean | AddEventListenerOptions,
 ): void
 function useEventListener(
-  element: Document | HTMLElement | Window | null | void,
+  element: Document | HTMLElement | Window | null | undefined,
   eventType: string,
   listener: (evt: Event) => void,
   options?: boolean | AddEventListenerOptions,
@@ -41,7 +41,7 @@ function useEventListener<
   KH extends keyof HTMLElementEventMap,
   KW extends keyof WindowEventMap
 >(
-  element: Document | HTMLElement | Window | null | void,
+  element: Document | HTMLElement | Window | null | undefined,
   eventType: KD | KH | KW | string,
   listener: (
     this: typeof element,
