5cd5405990f04c6e82c4787526d281daff6d1b50
Alex Young <foray1010@users.noreply.github.com>
2022-08-24 12:54:51 +0100

    chore: use @foray1010/jest-preset

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,7 @@
 import type { Config } from 'jest'
 
 const config: Config = {
-  collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.{cjs,cts,js,mjs,mts,ts,tsx}'],
-  coverageReporters: ['lcov', 'text-summary'],
+  preset: '@foray1010',
   testEnvironment: 'jest-environment-jsdom',
-  testMatch: ['**/*.{spec,test}.{cjs,cts,js,mjs,mts,ts,tsx}'],
 }
 export default config
