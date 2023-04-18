082c2addf99c8003d27c74908b29d7e2cab9cf5e
Alex Young <foray1010@users.noreply.github.com>
2023-04-18 15:14:39 +0100

    fixup! chore(deps): update dependency @foray1010/eslint-config to v10.5.0

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,35 @@
 import * as React from 'react'
 import { useDeepCompareMemo } from 'use-deep-compare'
 
+type Options = boolean | Readonly<AddEventListenerOptions> | undefined
+
 function useEventListener<KD extends keyof DocumentEventMap>(
-  element: Document | null | undefined,
+  element: Readonly<Document> | null | undefined,
   eventType: KD,
+  // eslint-disable-next-line functional/prefer-immutable-types
   listener: (this: Document, evt: DocumentEventMap[KD]) => void,
-  options?: boolean | AddEventListenerOptions | undefined,
+  options?: Options,
 ): void
 function useEventListener<KH extends keyof HTMLElementEventMap>(
-  element: HTMLElement | null | undefined,
+  element: Readonly<HTMLElement> | null | undefined,
   eventType: KH,
+  // eslint-disable-next-line functional/prefer-immutable-types
   listener: (this: HTMLElement, evt: HTMLElementEventMap[KH]) => void,
-  options?: boolean | AddEventListenerOptions | undefined,
+  options?: Options,
 ): void
 function useEventListener<KW extends keyof WindowEventMap>(
-  element: Window | null | undefined,
+  element: Readonly<Window> | null | undefined,
   eventType: KW,
+  // eslint-disable-next-line functional/prefer-immutable-types
   listener: (this: Window, evt: WindowEventMap[KW]) => void,
-  options?: boolean | AddEventListenerOptions | undefined,
+  options?: Options,
 ): void
 function useEventListener(
-  element: Document | HTMLElement | Window | null | undefined,
+  element: Readonly<Document | HTMLElement | Window> | null | undefined,
   eventType: string,
+  // eslint-disable-next-line functional/prefer-immutable-types
   listener: (evt: Event) => void,
-  options?: boolean | AddEventListenerOptions | undefined,
+  options?: Options,
 ): void
 
 function useEventListener<
@@ -31,17 +37,19 @@ function useEventListener<
   KH extends keyof HTMLElementEventMap,
   KW extends keyof WindowEventMap,
 >(
-  element: Document | HTMLElement | Window | null | undefined,
+  element: Readonly<Document | HTMLElement | Window> | null | undefined,
   eventType: KD | KH | KW | string,
   listener: (
+    /* eslint-disable functional/prefer-immutable-types */
     this: typeof element,
     evt:
       | DocumentEventMap[KD]
       | HTMLElementEventMap[KH]
       | WindowEventMap[KW]
       | Event,
+    /* eslint-enable functional/prefer-immutable-types */
   ) => void,
-  options?: boolean | AddEventListenerOptions | undefined,
+  options?: Options,
 ): void {
   const listenerRef = React.useRef(listener)
   listenerRef.current = listener
